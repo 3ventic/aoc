2e796ca3f00b60e98646615df9ec6f0c76704aaa
3v <ventic@3v.fi>
2020-12-01 19:37:39 +0100

    2020 day 1

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+const sumTarget = 2020
 
 func main() {
 	inputs := strings.Split(input, "\n")
@@ -17,5 +20,29 @@ func main() {
 		}
 	}
 
-	inpu
+	sort.Ints(inputNums)
+
+	parts, _ := findSumParts(inputNums, 2, sumTarget, 0)
+	log.Printf("Star 1: %d * %d = %d", parts[0], parts[1], parts[0]*parts[1])
+	parts, _ = findSumParts(inputNums, 3, sumTarget, 0)
+	log.Printf("Star 2: %d * %d * %d = %d", parts[0], parts[1], parts[2], parts[0]*parts[1]*parts[2])
+}
+
+func findSumParts(nums []int, parts int, target int, higherSum int) ([]int, bool) {
+	switch parts {
+	case 1:
+		for _, i := range nums {
+			if i+higherSum == target {
+				return []int{i}, true
+			}
+		}
+	default:
+		for _, i := range nums {
+			s, ok := findSumParts(nums, parts-1, target, i+higherSum)
+			if ok {
+				return append(s, i), true
+			}
+		}
+	}
+	return nil, false
 }
