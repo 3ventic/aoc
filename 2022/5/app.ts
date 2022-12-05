9cafab24999ff29e74c26af5d1e6f1e9b6bfb83f
3v <ventic@3v.fi>
2022-12-05 16:30:42 +0200

    cleanup

--- 2022/5/app.ts
+++ 2022/5/app.ts
@@ -2,8 +2,7 @@ import * as inputs from "./input";
 
 type State = {
 	moves: [number, number, number][];
-	stacks1: Record<number, string[]>;
-	stacks2: Record<number, string[]>;
+	stacks: Record<number, string[]>[];
 };
 
 function parseInput(input: string): State {
@@ -18,7 +17,7 @@ function parseInput(input: string): State {
 		.map((n) => parseInt(n, 10))
 		.pop()!;
 
-	const state = {
+	const state: State = {
 		moves: moveDef.map((line) => {
 			const match = /move (\d+) from (\d+) to (\d+)/.exec(line);
 			const ret: [number, number, number] = [
@@ -28,24 +27,23 @@ function parseInput(input: string): State {
 			];
 			return ret;
 		}),
-		stacks1: {},
-		stacks2: {},
+		stacks: [{}, {}],
 	};
 
 	for (let i = 1; i <= numOfStacks; i++) {
-		state.stacks1[i] = [];
-		state.stacks2[i] = [];
+		for (let j = 0; j < state.stacks.length; j++) {
+			state.stacks[j][i] = [];
+		}
 	}
 
 	for (let i = stackDef.length - 1; i >= 0; i--) {
-		let stack = 0;
-		for (let j = 0; j < stackDef[i].length; j++) {
-			if (j % 4 === 0) {
-				stack++;
+		for (let stack = 1; stack <= numOfStacks; stack++) {
+			const char = stackDef[i][stack * 4 - 3];
+			if (char === " ") {
+				continue;
 			}
-			if (/[A-Z]/.test(stackDef[i][j])) {
-				state.stacks1[stack].push(stackDef[i][j]);
-				state.stacks2[stack].push(stackDef[i][j]);
+			for (let j = 0; j < state.stacks.length; j++) {
+				state.stacks[j][stack].push(char);
 			}
 		}
 	}
@@ -57,17 +55,17 @@ const state = parseInput(inputs.input);
 for (const move of state.moves) {
 	const [crates, from, to] = move;
 	for (let i = 0; i < crates; i++) {
-		state.stacks1[to].push(state.stacks1[from].pop()!);
+		state.stacks[0][to].push(state.stacks[0][from].pop()!);
 	}
-	const moved = state.stacks2[from].splice(-crates);
-	state.stacks2[to].push(...moved);
+	const moved = state.stacks[1][from].splice(-crates);
+	state.stacks[1][to].push(...moved);
 }
 
 function printResult(part: number) {
 	console.log(
 		"Part",
 		part,
-		Object.values(state[`stacks${part}`])
+		Object.values(state.stacks[part - 1])
 			.map((s) => {
 				const stack = s as string[];
 				return stack[stack.length - 1];
