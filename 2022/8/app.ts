b7d65ae8d4e5b3e422b442ebbc77bb78084ff4f3
3v <ventic@3v.fi>
2022-12-08 11:17:47 +0200

    cleanup

--- 2022/8/app.ts
+++ 2022/8/app.ts
@@ -38,14 +38,14 @@ function scenicScores(graph: Tree[][]): [Tree, number] {
 	let edgeVisible = 0;
 	for (let x = 0; x < graph.length; x++) {
 		for (let y = 0; y < graph[x].length; y++) {
-			const tree = graph[y][x];
-			const row = graph[y];
-			const col = graph.map((row) => row[x]);
+			const tree = graph[x][y];
+			const row = graph[x];
+			const col = graph.map((row) => row[y]);
 			let scenicScore = 1;
-			scenicScore *= countVisible(tree, row.slice(x + 1));
-			scenicScore *= countVisible(tree, row.slice(0, x).reverse());
-			scenicScore *= countVisible(tree, col.slice(y + 1));
-			scenicScore *= countVisible(tree, col.slice(0, y).reverse());
+			scenicScore *= countVisible(tree, row.slice(y + 1));
+			scenicScore *= countVisible(tree, row.slice(0, y).reverse());
+			scenicScore *= countVisible(tree, col.slice(x + 1));
+			scenicScore *= countVisible(tree, col.slice(0, x).reverse());
 			tree.scenicScore = scenicScore;
 			if (tree.seesBeyondEdge) {
 				edgeVisible++;
