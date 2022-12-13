6568b362dd68c845f5606b916f0293a201f18620
3v <ventic@3v.fi>
2022-12-13 08:44:14 +0200

    cleanup

--- 2022/13/app.ts
+++ 2022/13/app.ts
@@ -10,40 +10,37 @@ function parseInput(input: string): SignalPair[] {
 	});
 }
 
-function compareOrder(signals: SignalPair): number {
-	const [a, b] = signals;
+function compareOrder(a: Signal, b: Signal): number {
 	for (let i = 0; i < a.length; i++) {
 		// A is longer than B, not valid signal
 		if (i >= b.length) {
-			return -1;
+			return 1;
 		}
-		if (isSignal(a[i]) && isSignal(b[i])) {
-			// @ts-ignore - it's being dumb about the type guard
-			const result = compareOrder([a[i], b[i]]);
+		const first = a[i];
+		const second = b[i];
+		if (isSignal(first) && isSignal(second)) {
+			const result = compareOrder(first, second);
 			if (result !== 0) {
 				return result;
 			}
-		} else if (isSignal(a[i])) {
-			// @ts-ignore - it's being dumb about the type guard
-			const result = compareOrder([a[i], [b[i]]]);
+		} else if (isSignal(first)) {
+			const result = compareOrder(first, [second]);
 			if (result !== 0) {
 				return result;
 			}
-		} else if (isSignal(b[i])) {
-			// @ts-ignore - it's being dumb about the type guard
-			const result = compareOrder([[a[i]], b[i]]);
+		} else if (isSignal(second)) {
+			const result = compareOrder([first], second);
 			if (result !== 0) {
 				return result;
 			}
-		} else if (a[i] === b[i]) {
+		} else if (first === second) {
 			continue;
 		} else {
-			// @ts-ignore - it's being dumb about the type guards
-			return b[i] - a[i];
+			return first - second;
 		}
 	}
 	if (a.length < b.length) {
-		return 1;
+		return -1;
 	}
 	return 0;
 }
@@ -56,7 +53,7 @@ const pairs = parseInput(inputs.input);
 console.log(
 	"Part 1:",
 	pairs.reduce((acc, pair, i) => {
-		if (compareOrder(pair) > 0) {
+		if (compareOrder(pair[0], pair[1]) < 0) {
 			return acc + i + 1;
 		} else return acc;
 	}, 0)
@@ -66,13 +63,13 @@ const signals: Signal[] = pairs.flat();
 
 const dividerPackets: Signal[] = [[[2]], [[6]]];
 signals.push(...dividerPackets);
-signals.sort((a, b) => compareOrder([b, a]));
+signals.sort(compareOrder);
 
 // Find index of divider packets
 const dividerIndices = signals.reduce((acc, signal, i) => {
-	if (compareOrder([signal, dividerPackets[0]]) === 0) {
+	if (compareOrder(signal, dividerPackets[0]) === 0) {
 		acc[0] = i + 1;
-	} else if (compareOrder([signal, dividerPackets[1]]) === 0) {
+	} else if (compareOrder(signal, dividerPackets[1]) === 0) {
 		acc[1] = i + 1;
 	}
 	return acc;
