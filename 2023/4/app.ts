b0451e8ee8fe47a235a73baf3d4bd7804ce466dc
3v <ventic@3v.fi>
2023-12-07 16:29:58 +0200

    2023d4

---
+++ 2023/4/app.ts
@@ -0,0 +1,36 @@
+import { input as inp } from "./input"
+
+type Game = { winningNumbers: number[]; gameNumbers: number[]; copies: number }
+
+const cards: Game[] = inp.split("\n").map((line) => {
+	const numbers = Array.from(line.matchAll(/(?:\d+|\|)(?!\d*:)/g))
+	const separatorIdx = numbers.findIndex((n) => n[0] === "|")
+	const winningNumbers = numbers.slice(0, separatorIdx).map((n) => +n[0])
+	const gameNumbers = numbers.slice(separatorIdx + 1).map((n) => +n[0])
+	return { winningNumbers, gameNumbers, copies: 1 }
+})
+
+function matchingNumbers(game: Game) {
+	return game.gameNumbers.filter((n) => game.winningNumbers.includes(n))
+}
+
+function gamePoints(game: Game) {
+	const points = game.gameNumbers.filter((n) => game.winningNumbers.includes(n))
+	return Math.floor(2 ** (points.length - 1))
+}
+
+console.log(
+	"Part 1:",
+	cards.reduce((a, b) => a + gamePoints(b), 0)
+)
+
+console.log(
+	"Part 2:",
+	cards.reduce((a, b, i, games) => {
+		const matches = matchingNumbers(b).length
+		for (let j = 1; j <= matches && j + i < games.length; j++) {
+			games[i + j].copies += b.copies
+		}
+		return a + b.copies
+	}, 0)
+)
