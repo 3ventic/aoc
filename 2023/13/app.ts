28d2278785f17fc00908ba1e81bb20e004a92aa9
3v <ventic@3v.fi>
2023-12-14 20:27:53 +0200

    2023d13

--- 2023/13/app.ts
+++ 2023/13/app.ts
@@ -4,24 +4,44 @@ const matrices = inp
 	.split("\n\n")
 	.map((s) => s.split("\n").map((l) => l.split("")))
 
-function findReflection(matrix: string[][]) {
+function findReflection(matrix: string[][], errors: number) {
 	yl: for (let y = 0; y < matrix.length - 1; y++) {
+		let errCounter = 0
 		for (let i = 0; true; i++) {
 			const row = matrix[y - i]
 			const row2 = matrix[y + 1 + i]
-			if (!row || !row2) return (y + 1) * 100
-			const match = row.every((c, x) => c === row2[x])
-			if (!match) continue yl
+			if (!row || !row2) {
+				if (errCounter === errors) {
+					return (y + 1) * 100
+				}
+				continue yl
+			}
+			let matches = 0
+			for (let x = 0; x < row.length; x++) {
+				if (row[x] === row2[x]) matches++
+			}
+			errCounter += row.length - matches
+			if (errCounter > errCounter) continue yl
 		}
 	}
 
 	xl: for (let x = 0; x < matrix[0].length - 1; x++) {
+		let errCounter = 0
 		for (let i = 0; true; i++) {
 			const col = matrix.map((row) => row[x - i])
 			const col2 = matrix.map((row) => row[x + 1 + i])
-			if (!col[0] || !col2[0]) return x + 1
-			const match = col.every((c, y) => c === col2[y])
-			if (!match) continue xl
+			if (!col[0] || !col2[0]) {
+				if (errCounter === errors) {
+					return x + 1
+				}
+				continue xl
+			}
+			let matches = 0
+			for (let y = 0; y < col.length; y++) {
+				if (col[y] === col2[y]) matches++
+			}
+			errCounter += col.length - matches
+			if (errCounter > errCounter) continue xl
 		}
 	}
 	return 0
@@ -29,5 +49,10 @@ function findReflection(matrix: string[][]) {
 
 console.log(
 	"Part 1:",
-	matrices.map(findReflection).reduce((a, b) => a + b)
+	matrices.map((m) => findReflection(m, 0)).reduce((a, b) => a + b)
+)
+
+console.log(
+	"Part 2:",
+	matrices.map((m) => findReflection(m, 1)).reduce((a, b) => a + b)
 )
